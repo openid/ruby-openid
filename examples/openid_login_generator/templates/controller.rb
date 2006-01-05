3bd61ce2979951b819b3c99a7026a87698a2ed5f
tailor <brian@janrain.com>
2006-01-05 00:02:32 +0000

    [project @ added openid_login_generator rails generator to examples]

---
+++ examples/openid_login_generator/templates/controller.rb
@@ -0,0 +1,73 @@
+require "openid/filestore"
+require "openid/consumer"
+
+store = OpenID::FilesystemOpenIDStore.new("/tmp/railsgen")
+$consumer = OpenID::OpenIDConsumer.new(store)
+
+class <%= class_name %>Controller < ApplicationController
+  layout  'scaffold'
+
+  def login
+    openid_url = @params[:identity_url]
+
+    if @request.post?
+      status, info = $consumer.beginAuth(openid_url)
+      case status
+      when OpenID::SUCCESS
+        return_to = url_for(:action=>:completeAuth, :token=>info.token)
+        trust_root = url_for(:controller=>"")
+        redirect_url = $consumer.constructRedirect(info, return_to, trust_root)
+
+        redirect_to(redirect_url)
+
+      when OpenID::PARSE_ERROR
+        flash["notice"] = "Could not find OpenID server for page #{openid_url}"
+        
+      when OpenID::HTTP_FAILURE
+        flash["notice"] = "Could not fetch page #{openid_url}"
+
+      end      
+    end    
+  end
+
+  def completeAuth
+    token = @params[:token]
+    
+    status, info = $consumer.completeAuth(token, @params)
+    
+    if status == OpenID::SUCCESS
+      openid_url = info
+
+      @user = User.get(openid_url)
+      
+      # create user object if one does not exist
+      if @user.nil?
+        @user = User.new(:openid_url => openid_url)
+        @user.save
+      end
+      @session[:user] = @user 
+
+      @session[:user] = openid_url
+      flash["notice"] = "Logged in as #{openid_url}"
+       
+      redirect_back_or_default :action => "welcome"
+      return
+
+    elsif status == OpenID::FAILURE and info
+      flash["notice"] = "Verification of #{info} failed."
+
+    else
+      flash["notice"] = "Verification cancelled"
+    end
+  
+    redirect_to :action => "login"
+  end
+  
+  def logout
+    @session[:user] = nil
+  end
+    
+  def welcome
+  end
+  
+end
