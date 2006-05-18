123276805089af650c51969aeb607b2b73d4c102
tailor <brian@janrain.com>
2006-05-18 18:22:13 +0000

    [project @ added unit test for the DumbStore and MemoryStore]

--- test/teststore.rb
+++ test/teststore.rb
@@ -21,3 +21,27 @@ class FileStoreTestCase < Test::Unit::TestCase
   end
 end
 
+class DumbStoreTestCase < Test::Unit::TestCase
+  include StoreTestCase
+
+  def setup
+    @store = OpenID::DumbStore.new('unit-test')
+  end
+
+  def test_nonce
+    assert_equal(true, @store.use_nonce('anything'))
+  end
+
+end
+
+class MemoryStoreTestCase < Test::Unit::TestCase
+  include StoreTestCase
+
+  @@dir = Pathname.new(Dir.tmpdir).join('filstoretest')
+
+  def setup
+    @store = OpenID::MemoryStore.new
+  end
+
+end
+
