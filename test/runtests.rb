b7f353d9ff39bb2d259c93e8c02acdc3d52c1437
tailor <brian@janrain.com>
2006-06-12 22:07:20 +0000

    [project @ silence test suite messages to stderr]

--- test/runtests.rb
+++ test/runtests.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/ruby
 
+# the tests exploit some corner cases which generate warning messages
+# on stderr.  try and silence those messages to avoid unnecessarily concerning
+# the library user.
+begin
+  STDERR.reopen('/dev/null', 'w')
+rescue
+  print ''
+end
+
 require "teststore"
 require "assoc"
 require "dh"
