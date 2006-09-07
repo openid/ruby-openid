68f38c8944ed83fc3e142513816e8e2979d23dd8
tailor <brian@janrain.com>
2006-09-07 01:05:25 +0000

    [project @ message unit tests and Message class bugfixes]

--- lib/openid/message.rb
+++ lib/openid/message.rb
@@ -32,9 +32,8 @@ module OpenID
   
   class Message
     
-    @@default_namesapaces = {
-      'sreg': SREG_URI
-    }
+    @@default_namesapaces = {'sreg' => SREG_URI}
+    @@allowed_openid_namespaces = [OPENID1_NS, OPENID2_NS]
 
     def initialize(openid_namspace=nil)
       @args = {}
@@ -51,11 +50,15 @@ module OpenID
       m = Message.new
       openid_args = {}
       args.each { |key,value|
-        if value.is_a? Array
+        if value.is_a?(Array)
           raise ArgumentError, 'query hash must have one value for each key, not lists of values.'
         end
         
         prefix, rest = key.split('.', 2)
+        if prefix.nil? or rest.nil?
+          prefix = nil
+          rest = nil
+        end
 
         if prefix != 'openid'
           @args[[BARE_NS,key].freeze] = value
@@ -81,14 +84,17 @@ module OpenID
       # resolve namespaces
       openid_args.each { |rest, value|
         ns_alias, ns_key = rest.split('.', 2)
-        # XXX: check port ValueError
+        if ns_alias.nil? or ns_key.nil?
+          ns_alias = NULL_NAMESPACE
+          ns_key = rest
+        end
 
         if ns_alias == 'ns'
           @namespaces.add_alias(value, ns_key)
         elsif ns_alias == NULL_NAMESPACE and ns_key == 'ns'
           @namespaces.add_alias(value, NULL_NAMESPACE)
         else
-          ns_args << [ns_alias, ns_key, value].freeze
+          ns_args << [ns_alias, ns_key, value]
         end
       }
 
@@ -97,7 +103,7 @@ module OpenID
       openid_ns_uri = OPENID1_NS unless openid_ns_uri
       
       self.set_openid_namespace(openid_ns_uri)
-      
+
       # put the pairs into the appropriate namespaces
       ns_args.each { |ns_alias, ns_key, value|
         ns_uri = @namespaces.get_namespace_uri(ns_alias)
@@ -105,9 +111,9 @@ module OpenID
           # only try to map an alias to a default if it's an
           # OpenID 1.x namespace
           if openid_ns_uri == OPENID1_NS
-            ns_uri = @@default_namesapaces.fetch(ns_alias)
+            ns_uri = @@default_namesapaces.fetch(ns_alias, nil)
           end
-
+          
           unless ns_uri
             ns_uri = openid_ns_uri
             ns_key = "#{ns_alias}.#{ns_key}"
@@ -120,7 +126,7 @@ module OpenID
     end
 
     def set_openid_namespace(openid_ns_uri)
-      if !self.allowed_openid_namespaces.include?(openid_ns_uri)
+      if !@@allowed_openid_namespaces.include?(openid_ns_uri)
         raise ArgumentError, "Invalid null namespace: #{openid_ns_uri}"
       end
       @namespaces.add_alias(openid_ns_uri, NULL_NAMESPACE)
@@ -156,7 +162,8 @@ module OpenID
         end
       }
 
-      args.each { |(ns_uri, ns_key), value|
+      @args.each { |k, value|
+        ns_uri, ns_key = k
         key = self.get_key(ns_uri, ns_key)
         args[key] = value
       }
@@ -183,7 +190,7 @@ module OpenID
     # message, to be HTTP POSTed as x-www-form-urlencoded UTF-8.
     def to_form_markup(action_url, form_tag_attrs=nil, submit_text='Continue')
       markup = "<form action='#{action_url}' method='POST' enctype='application/x-www-form-url-encoded' accept-charset='UTF-8'>\n"
-
+      
       self.to_post_args.each { |k,v|
         markup += "<input type='hidden' name='#{k}' value='#{v}' />\n"
       }
@@ -194,10 +201,10 @@ module OpenID
 
     # Generate a GET URL with the paramters in this message attacked as
     # query parameters.
-    def to_URL(base_url)
+    def to_url(base_url)
       return Util.append_args(base_url, self.to_post_args)
     end
-
+    
     # Generate a KVForm string that contains the parameters in this message.
     # This will fail is the message contains arguments outside of the 
     # "openid." prefix.
@@ -206,9 +213,9 @@ module OpenID
     end
     
     # Generate an x-www-urlencoded string.
-    def to_URL_encoded
+    def to_url_encoded
       args = self.to_post_args.map.sort
-      return Util.urlencoded(args)
+      return Util.urlencode(args)
     end
 
     # Convert an input value into the internally used values of this obejct.
@@ -222,11 +229,11 @@ module OpenID
       end
 
       if namespace != BARE_NS and namespace.class != String
-        raise ArgumentError, "Namespace must be BARE_NS, OPENID_NS or a string. Got #{namespace}".
+        raise ArgumentError, "Namespace must be BARE_NS, OPENID_NS or a string. Got #{namespace}"
       end
       
-      if namespace != BARE_NS and namespace.index(':').nil?
-        return SREG_URI if namespace == 'sreg'
+      if namespace != BARE_NS and namespace.index(':').nil? and namespace == 'sreg'
+        return SREG_URI
       end
 
       return namespace
@@ -241,12 +248,12 @@ module OpenID
     def get_key(namespace, ns_key)
       namespace = self.fix_ns(namespace)
       return ns_key if namespace == BARE_NS
-
-      ns_alias = @namespaces.get_alias(namespace)
       
-      # no alias is defined, so no key can exist
-      return nil unless ns_alias
+      ns_alias = @namespaces.get_alias(namespace)
 
+      # no alias is defined, so no key can exist
+      return nil if ns_alias.nil?
+      
       if ns_alias == NULL_NAMESPACE
         tail = ns_key
       else
@@ -255,18 +262,21 @@ module OpenID
       
       return 'openid.' + tail
     end
-
+    
     # Get a value for a namespaced key.
     def get_arg(namespace, key, defualt=nil)
       namespace = self.fix_ns(namespace)
       return @args.fetch([namespace,key], defualt)
     end
-
+    
     # Get the arguments that are defined for this namespace URI.
     def get_args(namespace)
       namespace = self.fix_ns(namespace)
       args = {}
-      @args.each {|(pair_ns,ns_key),v| args[ns_key] = v if pair_ns == namespace}
+      @args.each { |k,v|
+        pair_ns, ns_key = k
+        args[ns_key] = v if pair_ns == namespace
+      }
       return args
     end
     
@@ -275,28 +285,30 @@ module OpenID
       namespace = self.fix_ns(namespace)
       updates.each {|k,v| self.set_arg(namespace, k, v)}
     end
-
+    
     # Set a single argument in this namespace
     def set_arg(namespace, key, value)
       namespace = self.fix_ns(namespace)
       @args[[namespace, key].freeze] = value
       if namespace != BARE_NS
-        @namespace.add(namespace)
+        @namespaces.add(namespace)
       end
     end
-
+    
     # Remove a single argument from this namespace.
     def del_arg(namespace, key)
       namespace = self.fix_ns(namespace)
-      @args.delete([namespace,key].freeze)
+      _key = [namespace, key]
+      @args.delete(_key)
     end
-
+    
     def eql?(other)
       return @args == other.args
     end
-
+  
   end
 
+  
   # Maintains a bidirectional map between namespace URIs and aliases.
   class NamespaceMap
     
@@ -305,32 +317,32 @@ module OpenID
     # namespace map without an explicit desired name,
     # it will default to the value supplied here.
     @@default_aliases = {SREG_URI => 'sreg'}
-
+    
     def initialize
       @alias_to_namespace = {}
       @namespace_to_alias = {}
     end
-
+    
     def get_alias(namespace_uri)
-      @namespace_to_alias.fetch(namespace_uri)
+      @namespace_to_alias[namespace_uri]
     end
-
+    
     def get_namespace_uri(namespace_alias)
-      @alias_to_namespace.fetch(namespace_alias)
+      @alias_to_namespace[namespace_alias]
     end
-
+    
     # Add an alias from this namespace URI to the alias.
     def add_alias(namespace_uri, desired_alias)
       # check that there is not a namespace already defined for the
       # desired alias
-      current_namespace_uri = @alias_to_namespace.fetch(desired_alias)
+      current_namespace_uri = @alias_to_namespace.fetch(desired_alias, nil)
       if current_namespace_uri and current_namespace_uri != namespace_uri
         raise IndexError, "Cannot map #{namespace_uri} to alias #{desired_alias}. #{current_namespace_uri} is already mapped to alias #{desired_alias}"
       end
       
       # check that there is not already a (different) alias for this
       # namespace URI.
-      _alias = @namespace_to_alias.fetch(namespace_uri)
+      _alias = @namespace_to_alias[namespace_uri]
       if _alias and _alias != desired_alias
         raise IndexError, "Cannot map #{namespace_uri} to alias #{desired_alias}. It is already mapped to alias #{_alias}"
       end
@@ -339,16 +351,16 @@ module OpenID
       @namespace_to_alias[namespace_uri] = desired_alias
       return desired_alias
     end
-
+    
     # Add this namespace URI to the mapping, without caring what alias
     # it ends up with.
     def add(namespace_uri)
       # see if this namepace is already mapped to an alias
-      _alias = @namespace_to_alias.fetch(namespace_uri)
+      _alias = @namespace_to_alias[namespace_uri]
       return _alias if _alias
-
+      
       # see if there is a default alias for this namespace
-      default_alias = @@default_aliases.fetch(namespace_uri)
+      default_alias = @@default_aliases[namespace_uri]
       if default_alias
         begin
           self.add_alias(namespace_uri, default_alias)
@@ -358,7 +370,7 @@ module OpenID
           return default_alias
         end
       end
-
+      
       # Fall back to generating a numberical alias
       i = 0
       while true
@@ -371,14 +383,18 @@ module OpenID
           return _alias
         end
       end
-    
+      
       raise StandardError, 'Unreachable'
     end
-
+    
     def defined?(namespace_uri)
       @namespace_to_alias.has_key?(namespace_uri)
     end
+    
+    def each
+      @namespace_to_alias.each {|k,v| yield k,v}
+    end
 
   end
-
+  
 end
---
+++ test/message.rb
@@ -0,0 +1,399 @@
+require 'test/unit'
+require 'openid/message'
+
+class EmptyMessageTestCase < Test::Unit::TestCase
+  
+  def setup
+    @m = OpenID::Message.new
+  end
+
+  def test_to_post_args
+    assert_equal({}, @m.to_post_args)
+  end
+
+  def test_to_args
+    assert_equal({}, @m.to_args)
+  end
+
+  def test_to_kvform
+    assert_equal('', @m.to_kvform)
+  end
+
+  def test_to_url_encoded
+    assert_equal('', @m.to_url_encoded)
+  end
+
+  def test_to_url
+    base_url = 'http://base.url/'
+    assert_equal(base_url, @m.to_url(base_url))
+  end
+
+  def test_get_openid
+    assert_equal(nil, @m.get_openid_namespace)
+  end
+
+  def test_get_key_openid
+    assert_raise(OpenID::UndefinedOpenIDNamespace) { 
+      @m.get_key(OpenID::OPENID_NS, nil)
+    }
+  end
+
+  def test_get_key_bare
+    assert_equal('foo', @m.get_key(OpenID::BARE_NS, 'foo'))
+  end
+
+  def test_get_key_ns1
+    assert_equal(nil, @m.get_key(OpenID::OPENID1_NS, 'foo'))
+  end
+
+  def test_get_key_ns2
+    assert_equal(nil, @m.get_key(OpenID::OPENID2_NS, 'foo'))
+  end
+
+  def test_get_key_ns3
+    assert_equal(nil, @m.get_key('urn:something-special', 'foo'))
+  end
+
+  def test_has_key
+    assert_raise(OpenID::UndefinedOpenIDNamespace) {
+      @m.has_key?(OpenID::OPENID_NS, 'foo')
+    }
+  end
+
+  def test_has_key_bare
+    assert_equal(false, @m.has_key?(OpenID::BARE_NS, 'foo'))
+  end
+  
+  def test_has_key_ns1
+    assert_equal(false, @m.has_key?(OpenID::OPENID1_NS, 'foo'))
+  end
+
+  def test_has_key_ns2
+    assert_equal(false, @m.has_key?(OpenID::OPENID2_NS, 'foo'))
+  end
+
+  def test_has_key_ns3
+    assert_equal(false, @m.has_key?('urn:xxx', 'foo'))
+  end
+
+  def test_get_arg
+    assert_raise(OpenID::UndefinedOpenIDNamespace) {
+      @m.get_args(OpenID::OPENID_NS)
+    }
+  end
+
+  def test_get_arg_bare
+    assert_equal(nil, @m.get_arg(OpenID::BARE_NS, 'foo'))
+  end
+
+  def test_get_arg_ns1
+    assert_equal(nil, @m.get_arg(OpenID::OPENID1_NS, 'foo'))
+  end
+
+  def test_get_arg_ns2
+    assert_equal(nil, @m.get_arg(OpenID::OPENID2_NS, 'foo'))
+  end
+
+  def test_get_arg_ns3
+    assert_equal(nil, @m.get_arg('urn:xxx', 'foo'))
+  end
+
+  def test_get_args
+    assert_raise(OpenID::UndefinedOpenIDNamespace) {
+      @m.get_args(OpenID::OPENID_NS)
+    }
+  end
+
+  def test_get_args_bare
+    assert_equal({}, @m.get_args(OpenID::BARE_NS))
+  end
+
+  def test_get_args_ns1
+    assert_equal({}, @m.get_args(OpenID::OPENID1_NS))
+  end
+
+  def test_get_args_ns2
+    assert_equal({}, @m.get_args(OpenID::OPENID2_NS))
+  end
+
+  def test_get_args_ns3
+    assert_equal({}, @m.get_args('urn:xxx'))
+  end
+
+  def test_update_args
+    assert_raise(OpenID::UndefinedOpenIDNamespace) {
+      @m.update_args(OpenID::OPENID_NS, {'does not'=>'matter'})
+    }
+  end
+
+  def _test_update_args_ns(ns)
+    updates = {
+      'camper van beethoven' => 'david l',
+      'magnolia electric, co' => 'jason m'
+    }
+    assert_equal({}, @m.get_args(ns))
+    @m.update_args(ns, updates)
+    assert_equal(updates, @m.get_args(ns))
+  end
+
+  def test_update_args_bare
+    _test_update_args_ns(OpenID::BARE_NS)
+  end
+  def test_update_args_ns1
+    _test_update_args_ns(OpenID::OPENID1_NS)
+  end
+  def test_update_args_ns2
+    _test_update_args_ns(OpenID::OPENID2_NS)
+  end
+  def test_update_args_ns3
+    _test_update_args_ns('urn:xxx')
+  end
+
+  def test_set_arg
+    assert_raise(OpenID::UndefinedOpenIDNamespace) {
+      @m.set_arg(OpenID::OPENID_NS,'does not','matter')
+    }
+  end
+
+  def _test_set_arg_ns(ns)
+    key = 'Camper Van Beethoven'
+    value = 'David Lowery'
+    assert_equal(nil, @m.get_arg(ns, key))
+    @m.set_arg(ns, key, value)
+    assert_equal(value, @m.get_arg(ns, key))
+  end
+
+  def test_set_arg_bare
+    _test_set_arg_ns(OpenID::BARE_NS)
+  end
+  def test_set_arg_ns1
+    _test_set_arg_ns(OpenID::OPENID1_NS)
+  end
+  def test_set_arg_ns2
+    _test_set_arg_ns(OpenID::OPENID2_NS)
+  end
+  def test_set_arg_ns3
+    _test_set_arg_ns('urn:xxx')
+  end
+
+  def test_del_arg
+    assert_raise(OpenID::UndefinedOpenIDNamespace) {
+      @m.set_arg(OpenID::OPENID_NS, 'does not', 'matter')
+    }
+  end
+
+  def _test_del_arg_ns(ns)
+    key = 'Fleeting Joys'
+    assert_equal(nil, @m.del_arg(ns, key))
+  end
+
+  def test_del_arg_bare
+    _test_del_arg_ns(OpenID::BARE_NS)
+  end
+  def test_del_arg_ns1
+    _test_del_arg_ns(OpenID::OPENID1_NS)
+  end
+  def test_del_arg_ns2
+    _test_del_arg_ns(OpenID::OPENID2_NS)
+  end
+  def test_del_arg_ns3
+    _test_del_arg_ns('urn:xxx')
+  end
+
+end
+
+class OpenID1MessageTest < Test::Unit::TestCase
+  
+  def setup
+    @m = OpenID::Message.from_post_args({'openid.mode' => 'error',
+                                        'openid.error' => 'unit test'})
+  end
+  
+  def test_to_post_args
+    assert_equal({'openid.mode' => 'error',
+                   'openid.error' => 'unit test'},
+                 @m.to_post_args)
+  end
+
+  def test_to_args
+    assert_equal({'mode' => 'error',
+                   'error' => 'unit test'},
+                 @m.to_args)
+  end
+
+  def test_to_kvform
+    assert_equal("error:unit test\nmode:error\n",
+                 @m.to_kvform)
+  end
+
+  def test_to_url_encoded
+    assert_equal('openid.error=unit+test&openid.mode=error',
+                 @m.to_url_encoded)
+  end
+  
+  def test_to_url
+    base_url = 'http://base.url/'
+    actual = @m.to_url(base_url)
+    actual_base = actual[0...base_url.length]
+    assert_equal(base_url, actual_base)
+    assert_equal('?', actual[base_url.length].chr)
+    query = actual[base_url.length+1..-1]
+    assert_equal({'openid.mode'=>['error'],'openid.error'=>['unit test']},
+                 CGI.parse(query))
+  end
+
+  def test_get_openid
+    assert_equal(OpenID::OPENID1_NS, @m.get_openid_namespace)
+  end
+
+  def test_get_key_openid
+    assert_equal('openid.mode', @m.get_key(OpenID::OPENID_NS, 'mode'))
+  end
+
+  def test_get_key_bare
+    assert_equal('mode', @m.get_key(OpenID::BARE_NS, 'mode'))
+  end
+
+  def test_get_key_ns1
+    assert_equal('openid.mode', @m.get_key(OpenID::OPENID1_NS, 'mode'))
+  end
+
+  def test_get_key_ns2
+    assert_equal(nil, @m.get_key(OpenID::OPENID2_NS, 'mode'))
+  end
+
+  def test_get_key_ns2
+    assert_equal(nil, @m.get_key(OpenID::OPENID2_NS, 'mode'))
+  end
+
+  def test_get_key_ns3
+    assert_equal(nil, @m.get_key('urn:xxx', 'mode'))
+  end
+
+  def test_has_key
+    assert_equal(true, @m.has_key?(OpenID::OPENID_NS, 'mode'))
+  end
+  def test_has_key_bare
+    assert_equal(false, @m.has_key?(OpenID::BARE_NS, 'mode'))
+  end
+  def test_has_key_ns1
+    assert_equal(true, @m.has_key?(OpenID::OPENID1_NS, 'mode'))
+  end
+  def test_has_key_ns2
+    assert_equal(false, @m.has_key?(OpenID::OPENID2_NS, 'mode'))
+  end
+  def test_has_key_ns3
+    assert_equal(false, @m.has_key?('urn:xxx', 'mode'))
+  end
+
+  def test_get_arg
+    assert_equal('error', @m.get_arg(OpenID::OPENID_NS, 'mode'))
+  end
+  def test_get_arg_bare
+    assert_equal(nil, @m.get_arg(OpenID::BARE_NS, 'mode'))
+  end
+  def test_get_arg_ns1
+    assert_equal('error', @m.get_arg(OpenID::OPENID1_NS, 'mode'))
+  end
+  def test_get_arg_ns2
+    assert_equal(nil, @m.get_arg(OpenID::OPENID2_NS, 'mode'))
+  end
+  def test_get_arg_ns3
+    assert_equal(nil, @m.get_arg('urn:xxx', 'mode'))
+  end
+
+  def test_get_args
+    assert_equal({'mode'=>'error','error'=>'unit test'},
+                 @m.get_args(OpenID::OPENID_NS))
+  end
+  def test_get_args_bare
+    assert_equal({}, @m.get_args(OpenID::BARE_NS))
+  end
+  def test_get_args_ns1
+    assert_equal({'mode'=>'error','error'=>'unit test'},
+                 @m.get_args(OpenID::OPENID1_NS))
+  end
+  def test_get_args_ns2
+    assert_equal({}, @m.get_args(OpenID::OPENID2_NS))
+  end
+  def test_get_args_ns3
+    assert_equal({}, @m.get_args('urn:xxx'))
+  end
+
+  def _test_update_args_ns(ns, before=nil)
+    if before.nil?
+      before = {}
+    end
+    update_args = {
+      'Camper van Beethoven'=>'David Lowery',
+      'Magnolia Electric Co.'=>'Jason Molina'
+    }
+    assert_equal(before, @m.get_args(ns))
+    @m.update_args(ns, update_args)
+    after = before.dup
+    after.update(update_args)
+    assert_equal(after, @m.get_args(ns))
+  end
+
+  def test_update_args
+    _test_update_args_ns(OpenID::OPENID_NS, {'mode'=>'error','error'=>'unit test'})
+  end
+  def test_update_args_bare
+    _test_update_args_ns(OpenID::BARE_NS)
+  end
+  def test_update_args_ns1
+    _test_update_args_ns(OpenID::OPENID1_NS, {'mode'=>'error','error'=>'unit test'})
+  end
+  def test_update_args_ns2
+    _test_update_args_ns(OpenID::OPENID2_NS)
+  end
+  def test_update_args_ns3
+    _test_update_args_ns('urn:xxx')
+  end
+
+  def _test_set_arg_ns(ns)
+    key = 'awesometown'
+    value = 'funny'
+    assert_equal(nil, @m.get_arg(ns,key))
+    @m.set_arg(ns, key, value)
+    assert_equal(value, @m.get_arg(ns,key))
+  end
+
+  def test_set_arg; _test_set_arg_ns(OpenID::OPENID_NS); end
+  def test_set_arg_bare; _test_set_arg_ns(OpenID::BARE_NS); end
+  def test_set_arg_ns1; _test_set_arg_ns(OpenID::OPENID1_NS); end
+  def test_set_arg_ns2; _test_set_arg_ns(OpenID::OPENID2_NS); end
+  def test_set_arg_ns3; _test_set_arg_ns('urn:xxx'); end
+
+  def _test_del_arg_ns(ns)
+    key = 'marry an'
+    value = 'ice cream sandwich'
+    @m.set_arg(ns, key, value)
+    assert_equal(value, @m.get_arg(ns,key))
+    @m.del_arg(ns,key)
+    assert_equal(nil, @m.get_arg(ns,key))
+  end
+
+  def test_del_arg; _test_del_arg_ns(OpenID::OPENID_NS); end
+  def test_del_arg_bare; _test_del_arg_ns(OpenID::BARE_NS); end
+  def test_del_arg_ns1; _test_del_arg_ns(OpenID::OPENID1_NS); end
+  def test_del_arg_ns2; _test_del_arg_ns(OpenID::OPENID2_NS); end
+  def test_del_arg_ns3; _test_del_arg_ns('urn:yyy'); end
+
+end
+
+class NamespaceMapTestCase < Test::Unit::TestCase
+  
+  def test_onealias
+    nsm = OpenID::NamespaceMap.new
+    uri = 'http://example.com/foo'
+    _alias = 'foo'
+    nsm.add_alias(uri, _alias)
+    assert_equal(uri, nsm.get_namespace_uri(_alias))
+    assert_equal(_alias, nsm.get_alias(uri))
+  end
+
+  
+
+
+end
