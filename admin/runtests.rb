c4e99cf4c15b846d4997f9035340bc3df49777cb
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-19 21:07:31 +0000

    [project @ Move test collector to admin and add driver (admin/runtests"]

--- admin/runtests.rb
+++ admin/runtests.rb
@@ -1,23 +1,17 @@
 #!/usr/bin/ruby
 
-require "openid/util"
 require "logger"
 require "stringio"
-
-# Redirect logging output to a buffer.
-logfile = StringIO.new
-OpenID::Util.setLogger(Logger.new(logfile))
+require "pathname"
 
 require 'test/unit'
 require 'test/unit/collector/dir'
 
+tests_dir = Pathname.new(__FILE__).dirname.dirname.join('test')
+
 # Collect tests from everything named test_*.rb.
 c = Test::Unit::Collector::Dir.new
-suite = c.collect
+suite = c.collect tests_dir
 
 require 'test/unit/ui/console/testrunner'
 Test::Unit::UI::Console::TestRunner.run(suite)
-
-# Dump the logs.
-#logfile.rewind
-#puts logfile.read
