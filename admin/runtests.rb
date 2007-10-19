49083ecc33eb41397455f7101c8a8568f552a327
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-19 22:15:50 +0000

    [project @ Test runner now exits with failure if the tests did not pass]

--- admin/runtests.rb
+++ admin/runtests.rb
@@ -4,14 +4,18 @@ require "logger"
 require "stringio"
 require "pathname"
 
-require 'test/unit'
 require 'test/unit/collector/dir'
-
-tests_dir = Pathname.new(__FILE__).dirname.dirname.join('test')
-
-# Collect tests from everything named test_*.rb.
-c = Test::Unit::Collector::Dir.new
-suite = c.collect tests_dir
-
 require 'test/unit/ui/console/testrunner'
-Test::Unit::UI::Console::TestRunner.run(suite)
+
+def main
+    tests_dir = Pathname.new(__FILE__).dirname.dirname.join('test')
+
+    # Collect tests from everything named test_*.rb.
+    c = Test::Unit::Collector::Dir.new
+    suite = c.collect tests_dir
+
+    result = Test::Unit::UI::Console::TestRunner.run(suite)
+    result.passed?
+end
+
+exit(main)
