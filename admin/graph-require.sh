285b6c4a949e3259bb35d165f939ba6555464773
tailor <cygnus@janrain.com>
2007-10-22 17:56:30 +0000

    [project @ Update deps graphing script to accept lib directory and output filename]

--- admin/graph-require.sh
+++ admin/graph-require.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-grep -r '^ *require ['"'"'"]' lib/ > require.txt
+
+OUTPUT_FILE="deps.jpg"
+
+if [ ! "$1" ] ; then
+  echo "Usage: graph-require.sh <lib_directory> [output_filename]"
+  exit 1
+fi
+
+if [ "$2" ] ; then
+  OUTPUT_FILE=$2
+fi
+
+grep -r '^ *require ['"'"'"]' $1 > require.txt
 
 python <<EOF
 import re
@@ -11,5 +23,5 @@ parse_require = re.compile(
     re.MULTILINE)
 matches = parse_require.findall(file('require.txt').read())
 g = pydot.graph_from_edges(matches, directed=True)
-g.write_jpeg('deps.jpg')
+g.write_jpeg('$OUTPUT_FILE')
 EOF
