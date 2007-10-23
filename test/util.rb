f18ca730d7e6560c81a151906415baa9d460c173
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-23 21:36:05 +0000

    [project @ Make assert_log_matches work for more than one line of log output]

--- test/util.rb
+++ test/util.rb
@@ -3,14 +3,21 @@ require 'stringio'
 
 module OpenID
   module TestUtil
-    def assert_log_matches(regex)
-      old_logger = Util.logger
-      log_output = StringIO.new
-      Util.logger = Logger.new(log_output)
-      yield
-      assert_match(regex, log_output.string)
-    ensure
-      Util.logger = old_logger
+    def assert_log_matches(*regexes)
+      begin
+        old_logger = Util.logger
+        log_output = StringIO.new
+        Util.logger = Logger.new(log_output)
+        yield
+      ensure
+        Util.logger = old_logger
+      end
+      log_output.rewind
+      log_lines = log_output.readlines
+      assert_equal(regexes.length, log_lines.length)
+      log_output.readlines.zip(regexes) do |line, regex|
+        assert_match(regex, line)
+      end
     end
   end
 end
