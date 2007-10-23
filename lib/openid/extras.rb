2c7ee4b222d0902db6efaad54f98339363d903fa
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-23 22:45:25 +0000

    [project @ Remove unused code from openid/extras]

--- lib/openid/extras.rb
+++ lib/openid/extras.rb
@@ -1,26 +1,11 @@
-
-class Object
-
-  def instance_variable_hash
-    h = {}
-    self.instance_variables.each { |k| h[k] = self.instance_variable_get(k) }
-    return h
-  end
-
-end
-
 class String
-
   def starts_with?(other)
-    other = other.to_str
     head = self[0, other.length]
     head == other
   end
 
   def ends_with?(other)
-    other = other.to_str
     tail = self[-1 * other.length, other.length]
     tail == other
   end
-
 end
