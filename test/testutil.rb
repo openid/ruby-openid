cdf4e59ecad437d2a4de32ffd377a96fee150201
tailor <cygnus@janrain.com>
2007-10-23 20:25:47 +0000

    [project @ Make read_data_file read file as lines or a single string]

--- test/testutil.rb
+++ test/testutil.rb
@@ -1,6 +1,14 @@
 
+require "pathname"
+
 TESTS_DIR = Pathname.new(__FILE__).dirname
 
-def read_data_file(filename)
-  File.readlines(TESTS_DIR.join('data', filename).to_s)
+def read_data_file(filename, lines=true)
+  fname = TESTS_DIR.join('data', filename).to_s
+
+  if lines
+    File.readlines(fname)
+  else
+    File.read(fname)
+  end
 end
