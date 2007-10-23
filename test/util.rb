2d1f1082fecfb840daf7e20eb781d0d32fa2d35f
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-23 21:15:06 +0000

    [project @ Added mixin that has assert_log_matches method]

---
+++ test/util.rb
@@ -0,0 +1,16 @@
+# Utilities that are only used in the testing code
+
+module OpenID
+  module TestUtil
+    def assert_log_matches(regex)
+      old_logger = Util.logger
+      log_output = StringIO.new
+      Util.logger = Logger.new(log_output)
+      yield
+      assert_match(regex, log_output.string)
+    ensure
+      Util.logger = old_logger
+    end
+  end
+end
+
