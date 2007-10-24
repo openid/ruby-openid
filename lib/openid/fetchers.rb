2ee8535e01911863ee004dd99ad3421f1e28aaf3
Kevin Turner <kevin@janrain.com>
2007-10-24 00:40:37 +0000

    [project @ fetchers: add final_url method to HTTPResponse and follow redirects.]

--- lib/openid/fetchers.rb
+++ lib/openid/fetchers.rb
@@ -1,9 +1,35 @@
 require 'net/http'
 
 module OpenID
+  # Our HTTPResponse class extends Net::HTTPResponse with an additional
+  # method, final_url.
+  class HTTPResponse
+    attr_accessor :final_url
+
+    attr_accessor :_response
+
+    def self._from_net_response(response, final_url)
+      me = self.new
+      me._response = response
+      me.final_url = final_url
+      return me
+    end
+
+    def method_missing(method, *args)
+      @_response.send(method, *args)
+    end
+  end
+
   class StandardFetcher
     def fetch(url, body=nil, headers=nil)
-      Net::HTTP.get_response(url)
+      response = Net::HTTP.get_response(url)
+      case response
+      when Net::HTTPRedirection
+        redirect_url = URI.parse(response["location"])
+        return fetch(redirect_url, body, headers)
+      else
+        return HTTPResponse._from_net_response(response, url)
+      end
     end
   end
 end
--- test/test_fetchers.rb
+++ test/test_fetchers.rb
@@ -7,28 +7,46 @@ require 'openid/fetchers'
 require 'stringio'
 
 module HttpResultAssertions
-  def assert_http_result_is(expected_code, result)
-    assert_equal expected_code.to_s, result.code
-    assert_equal "the expected body", result.body
+  def assert_http_result_is(expected, result)
+    assert_equal expected.code, result.code
+    assert_equal expected.body, result.body
+    assert_equal expected.final_url, result.final_url
   end
 end
+
 
 class FetcherTestCase < Test::Unit::TestCase
   include HttpResultAssertions
 
+  class ExpectedResponse < Net::HTTPResponse
+    attr_reader :final_url
+
+    def initialize(code, final_url, body="the expected body",
+                   httpv="1.1", msg=nil)
+      super(httpv, code, msg)
+      @code = code
+      @body = body
+      @final_url = final_url
+    end
+
+    def body
+      @body
+    end
+  end
 
   @@cases =
     [
-     ['/success', 200],
-#      ['/301redirect', 200],
-#      ['/302redirect', 200],
-#      ['/303redirect', 200],
-#      ['/307redirect', 200],
-     ['/notfound', 404],
-     ['/badreq', 400],
-     ['/forbidden', 403],
-     ['/error', 500],
-     ['/server_error', 503],
+     # path, status code, expected url (nil = default to path)
+     ['/success', 200, nil],
+     ['/notfound', 404, nil],
+     ['/badreq', 400, nil],
+     ['/forbidden', 403, nil],
+     ['/error', 500, nil],
+     ['/server_error', 503, nil],
+     ['/301redirect', 200, '/success'],
+     ['/302redirect', 200, '/success'],
+     ['/303redirect', 200, '/success'],
+     ['/307redirect', 200, '/success'],
     ]
 
   def _redirect_with_code(code)
@@ -45,9 +63,6 @@ class FetcherTestCase < Test::Unit::TestCase
     }
   end
 
-  class HarnessBroke < Exception
-  end
-
   def setup
     @fetcher = OpenID::StandardFetcher.new
     @logfile = StringIO.new
@@ -57,7 +72,10 @@ class FetcherTestCase < Test::Unit::TestCase
                                       :AccessLog => [])
     @server_thread = Thread.new {
       @server.mount_proc('/success', _respond_with_code(200))
-      # @server.mount_proc('/301redirect', _redirect_with_code(301))
+      @server.mount_proc('/301redirect', _redirect_with_code(301))
+      @server.mount_proc('/302redirect', _redirect_with_code(302))
+      @server.mount_proc('/303redirect', _redirect_with_code(303))
+      @server.mount_proc('/307redirect', _redirect_with_code(307))
       @server.mount_proc('/badreq', _respond_with_code(400))
       @server.mount_proc('/forbidden', _respond_with_code(403))
       @server.mount_proc('/notfound', _respond_with_code(404))
@@ -81,16 +99,35 @@ class FetcherTestCase < Test::Unit::TestCase
     @server_thread.join
   end
 
+  def test_final_url_tainted
+    uri = _uri_build('/301redirect')
+    result = @fetcher.fetch(uri)
+    assert result.final_url.host.tainted?
+    assert result.final_url.path.tainted?
+  end
+
   def test_cases
-    for path, expected_code in @@cases
+    for path, expected_code, expected_url in @@cases
+      uri = _uri_build(path)
+      if expected_url.nil?
+        expected_url = uri
+      else
+        expected_url = _uri_build(expected_url)
+      end
+
+      expected = ExpectedResponse.new(expected_code.to_s, expected_url)
+      result = @fetcher.fetch(uri)
+
       begin
-        uri = _uri_build(path)
-        result = @fetcher.fetch(uri)
-        assert_http_result_is expected_code, result
+        assert_http_result_is expected, result
       rescue Test::Unit::AssertionFailedError => err
         if result.code == '500' && expected_code != 500
           # Looks like our WEBrick harness broke.
-          raise "Status #{result.code} from case #{path}.  Logs:\n#{@logfile.string}"
+          msg = <<EOF
+Status #{result.code} from case #{path}.  Logs:
+#{@logfile.string}
+EOF
+          raise msg
         end
 
         # Wrap failure messages so we can tell which case failed.
