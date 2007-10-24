369ba48a8601297e89ec70ddee7d90a994108a3e
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-24 20:23:05 +0000

    [project @ Naming for attributes and arguments of DiffieHellman object]

--- lib/openid/dh.rb
+++ lib/openid/dh.rb
@@ -13,20 +13,20 @@ module OpenID
     @@default_mod = 155172898181473697471232257763715539915724801966915404479707795314057629378541917580651227423698188993727816152646631438561595825688188889951272158842675419950341258706556549803580104870537681476726513255747040765857479291291572334510643245094715007229621094194349783925984760375594985848253359305585439638443
     @@default_gen = 2
 
-    attr_reader :p, :g, :public
+    attr_reader :modulus, :generator, :public
 
     def DiffieHellman.from_defaults
       DiffieHellman.new(@@default_mod, @@default_gen)
     end
 
-    def initialize(p=nil, g=nil)
-      @p = p.nil? ? @@default_mod : p
-      @g = g.nil? ? @@default_gen : g
-      set_private(OpenID::CryptUtil.rand(@p-2) + 1)
+    def initialize(modulus=nil, generator=nil, priv=nil)
+      @modulus = modulus.nil? ? @@default_mod : modulus
+      @generator = generator.nil? ? @@default_gen : generator
+      set_private(priv.nil? ? OpenID::CryptUtil.rand(@modulus-2) + 1 : priv)
     end
 
     def get_shared_secret(composite)
-      DiffieHellman.powermod(composite, @private, @p)
+      DiffieHellman.powermod(composite, @private, @modulus)
     end
 
     def xor_secret(algorithm, composite, secret)
@@ -39,7 +39,7 @@ module OpenID
     private
     def set_private(priv)
       @private = priv
-      @public = DiffieHellman.powermod(@g, @private, @p)
+      @public = DiffieHellman.powermod(@generator, @private, @modulus)
     end
 
     def DiffieHellman.strxor(s, t)
