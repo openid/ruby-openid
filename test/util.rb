cf1cc811d3546f29ed15526a35565d8f06639f11
tailor <cygnus@janrain.com>
2007-10-24 22:01:00 +0000

    [project @ Add extra log-checking methods]

--- test/util.rb
+++ test/util.rb
@@ -14,9 +14,35 @@ module OpenID
       end
       log_output.rewind
       log_lines = log_output.readlines
-      assert_equal(regexes.length, log_lines.length)
+      assert_equal(regexes.length, log_lines.length,
+                   [regexes, log_lines].inspect)
       log_lines.zip(regexes) do |line, regex|
         assert_match(regex, line)
+      end
+    end
+
+    def assert_log_line_count(num_lines)
+      begin
+        old_logger = Util.logger
+        log_output = StringIO.new
+        Util.logger = Logger.new(log_output)
+        yield
+      ensure
+        Util.logger = old_logger
+      end
+      log_output.rewind
+      log_lines = log_output.readlines
+      assert_equal(num_lines, log_lines.length)
+    end
+
+    def silence_logging
+      begin
+        old_logger = Util.logger
+        log_output = StringIO.new
+        Util.logger = Logger.new(log_output)
+        yield
+      ensure
+        Util.logger = old_logger
       end
     end
   end
