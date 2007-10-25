10615cc533b124884bc347315a40f3eb18725bb5
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-10-25 18:36:08 +0000

    [project @ Documentation for dh]

--- lib/openid/dh.rb
+++ lib/openid/dh.rb
@@ -11,11 +11,14 @@ module OpenID
 
   class DiffieHellman
 
+    # From the OpenID specification
     @@default_mod = 155172898181473697471232257763715539915724801966915404479707795314057629378541917580651227423698188993727816152646631438561595825688188889951272158842675419950341258706556549803580104870537681476726513255747040765857479291291572334510643245094715007229621094194349783925984760375594985848253359305585439638443
     @@default_gen = 2
 
     attr_reader :modulus, :generator, :public
 
+    # A new DiffieHellman object, using the modulus and generator from
+    # the OpenID specification
     def DiffieHellman.from_defaults
       DiffieHellman.new(@@default_mod, @@default_gen)
     end
