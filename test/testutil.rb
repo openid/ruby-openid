23f8ab3e5c8d392aa3a1e8ca8bf193a143173b7a
Kevin Turner <kevin@janrain.com>
2007-10-26 23:07:43 +0000

    [project @ TestDataMixin#read_data_file: parameterize data_dir]

--- test/testutil.rb
+++ test/testutil.rb
@@ -3,14 +3,15 @@ require "pathname"
 module OpenID
   module TestDataMixin
     TESTS_DIR = Pathname.new(__FILE__).dirname
-
-    def read_data_file(filename, lines=true)
-      fname = TESTS_DIR.join('data', filename).to_s
+    TEST_DATA_DIR = Pathname.new('data')
+    
+    def read_data_file(filename, lines=true, data_dir=TEST_DATA_DIR)
+      fname = TESTS_DIR.join(data_dir, filename)
 
       if lines
-        File.readlines(fname)
+        fname.readlines
       else
-        File.read(fname)
+        fname.read
       end
     end
   end
