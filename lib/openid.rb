9e530336c1ce2de44543261e76e9818d5998bf74
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2007-11-16 03:09:32 +0000

    [project @ Move code from openid.rb into util]

--- lib/openid.rb
+++ lib/openid.rb
@@ -1,13 +1,2 @@
 require "openid/consumer"
 require 'openid/server'
-
-# See OpenID::Consumer or OpenID::Server modules, as well as the store classes
-module OpenID
-  VERSION = "2.0.0-dev"
-
-  # Exceptions that are raised by the library are subclasses of this
-  # exception type, so if you want to catch all exceptions raised by
-  # the library, you can catch OpenIDError
-  class OpenIDError < StandardError
-  end
-end
--- lib/openid/util.rb
+++ lib/openid/util.rb
@@ -7,8 +7,17 @@ require "openid/extras"
 
 srand(Time.now.to_f)
 
+# See OpenID::Consumer or OpenID::Server modules, as well as the store classes
 module OpenID
   class AssertionError < Exception
+  end
+
+  VERSION = "2.0.0-dev"
+
+  # Exceptions that are raised by the library are subclasses of this
+  # exception type, so if you want to catch all exceptions raised by
+  # the library, you can catch OpenIDError
+  class OpenIDError < StandardError
   end
 
   module Util
