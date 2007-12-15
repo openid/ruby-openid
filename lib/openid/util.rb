9744c1ea5e4e33b10bf8b82bd7fffaaf7cec6925
Kevin Turner <kevin@janrain.com>
2007-12-15 00:30:49 +0000

    [project @ set version number to 2.0.1]

--- lib/openid/util.rb
+++ lib/openid/util.rb
@@ -12,7 +12,7 @@ module OpenID
   class AssertionError < Exception
   end
 
-  VERSION = "2.0.0"
+  VERSION = "2.0.1"
 
   # Exceptions that are raised by the library are subclasses of this
   # exception type, so if you want to catch all exceptions raised by
