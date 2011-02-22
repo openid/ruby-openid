49f838b20aa681b4b888b8196ae34c7a128bd52f
mmell <mike@nthwave.net>
2011-02-21 18:10:28 -0800

    Allow setting Yadis::XRI::ProxyResolver::DEFAULT_PROXY premptively, outside of the class definition. Useful for iname-based communities running their own resolver.

--- lib/openid/yadis/xrires.rb
+++ lib/openid/yadis/xrires.rb
@@ -13,7 +13,7 @@ module OpenID
 
       class ProxyResolver
 
-        DEFAULT_PROXY = 'http://proxy.xri.net/'
+        DEFAULT_PROXY ||= 'http://proxy.xri.net/'
 
         def initialize(proxy_url=nil)
           if proxy_url
