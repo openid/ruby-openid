053324f1ffa18277f80d18ca02f6cfddbb544bde
Akira Matsuda <ronnie@dio.jp>
2013-06-19 20:18:37 +0900

    Stop overwriting String#starts_with? and String#ends_with?

--- lib/openid/extras.rb
+++ lib/openid/extras.rb
@@ -3,11 +3,11 @@ class String
     other = other.to_s
     head = self[0, other.length]
     head == other
-  end
+  end unless ''.respond_to?(:starts_with?)
 
   def ends_with?(other)
     other = other.to_s
     tail = self[-1 * other.length, other.length]
     tail == other
-  end
+  end unless ''.respond_to?(:ends_with?)
 end
